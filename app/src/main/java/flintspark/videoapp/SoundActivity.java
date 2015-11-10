8cc56af0f89d76939114203bf0a8b6be7c914d49
Traitortots <jker3169@740s-pl-122.local>
2015-11-10 09:57:36 -0700

    added to github

---
+++ app/src/main/java/flintspark/videoapp/SoundActivity.java
@@ -0,0 +1,111 @@
+package flintspark.videoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.*;
+
+public class SoundActivity extends AppCompatActivity implements Runnable
+{
+    private Button startButton;
+    private Button stopButton;
+    private Button pauseButton;
+    private Button videoButton;
+    private MediaPlayer soundPlayer;
+    private SeekBar soundSeekBar;
+    private Thread soundThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound);
+
+        startButton = (Button) findViewById(R.id.playButton);
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
+        soundSeekBar = (SeekBar) findViewById(R.id.soundSeekBar);
+        videoButton = (Button) findViewById(R.id.videoButton);
+        soundPlayer = MediaPlayer.create(this.getBaseContext(), R.raw.fnaf);
+
+        setupListeners();
+
+        soundThread = new Thread(this);
+        soundThread.start();
+
+    }
+
+    private void setupListeners()
+    {
+        startButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                soundPlayer.start();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                soundPlayer.pause();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                soundPlayer.stop();
+                soundPlayer = MediaPlayer.create(getBaseContext(), R.raw.fnaf);
+            }
+        });
+
+        videoButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View currentView)
+            {
+                Intent myIntent = new Intent(currentView.getContext(), VideoActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        soundSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                if (fromUser)
+                {
+                    soundPlayer.seekTo(progress);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void run()
+    {
+        int currentPosition = 0;
+        int soundTotal = soundPlayer.getDuration();
+        soundSeekBar.setMax(soundTotal);
+
+        while (soundPlayer != null && currentPosition < soundTotal)
+        {
+
+        }
+    }
+}
